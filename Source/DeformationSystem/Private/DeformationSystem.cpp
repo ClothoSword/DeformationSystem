13e14d7ec44e2b62fbcd90c9093a7a540bcd9a9d
ryu <2832585107@qq.com>
2022-08-09 23:07:41 +0800

    init repo

---
+++ Source/DeformationSystem/Private/DeformationSystem.cpp
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DeformationSystem.h"
+#include "Interfaces/IPluginManager.h"
+
+#define LOCTEXT_NAMESPACE "FDeformationSystemModule"
+
+void FDeformationSystemModule::StartupModule()
+{
+	FString PluginShaderDir = FPaths::Combine(IPluginManager::Get().FindPlugin(TEXT("DeformationSystem"))->GetBaseDir(), TEXT("Shaders"));
+	AddShaderSourceDirectoryMapping(TEXT("/Plugin/DeformationSystem"), PluginShaderDir);
+}
+
+void FDeformationSystemModule::ShutdownModule()
+{}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FDeformationSystemModule, DeformationSystem)
